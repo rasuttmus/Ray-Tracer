a9edfbb955813697d28c73839f5a79c37ef77c71
Rasuttmus <rasmus270@hotmail.com>
2014-10-14 10:53:09 +0200

    first commit yoo

---
+++ main.cpp
@@ -0,0 +1,98 @@
+//Classes for Ray tracing project in TNCG15
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include "glm/glm.hpp"
+
+
+using namespace std;
+
+
+
+class Camera{
+public:
+	//Instance vairables
+	glm::vec3 position;
+	glm::vec3 viewDirection;
+	float viewPlaneDistance;
+	int raysPerPixel;
+	//vector<*Pixel> pixels;
+
+	//construtor
+
+	//Instance methods
+	void renderImage();
+	void mappingFunction();
+	void displayImage();
+
+
+};
+
+class Pixel{
+public:
+	//Instance variables
+	//vector<&Ray> rays;
+	glm::vec3 colorOfPixel;
+	//constructor
+
+	//Instance methods
+	void shootingRays();
+
+};
+
+class Ray{
+public:
+	//Instance variables
+	glm::vec3 startingPoint;
+	glm::vec3 direction;
+	float importance;
+	glm::vec3 color;
+	bool finalNode;
+	//var *childrenNodes;
+	bool insideObject;
+
+
+	//constructor
+
+	//Instance methods	
+	glm::vec3 calculateColor();
+	glm::vec3 calculateLocalContribution();
+
+};
+
+class Sphere{
+public:
+	//Instance variables
+	glm::vec3 position;
+	float radius;
+	bool transparency;	
+	float refractiveIndex;
+
+	//contructor
+
+	//Instance methods
+
+};
+
+class Rectangle{};
+
+class Cube{};
+
+class Wall{};
+
+class Light{};
+
+
+
+int main(int argc, char **argv)
+{
+
+	cout << "\n\n>>>---A little render message--->\n\n";
+
+
+	return 0;
+}
